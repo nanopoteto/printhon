2c02ec0c1da1f5c6fc767cba151440556ca9d38f
shugoyamaguchi <yshgo91@gmail.com>
2018-09-27 12:58:39 -0700

    edit_comment

--- printhon/namevalue.py
+++ printhon/namevalue.py
@@ -2,8 +2,6 @@ import inspect
 import re
 
 def printnv(input, con=''):
-    # print inspect.currentframe().f_back.f_restricted
     context = inspect.getframeinfo(inspect.currentframe().f_back).code_context[0]
-    # text = context.split('(')[1].split(')')[0]
     text = re.search(r'\(.*\)', context).group()[1:-1].split(',')[0]
     print(text+con, input)
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import printhon as pp
 
 pp.printb('hoge')
 
-a = (1,1,2,3,5)
+a = (1, 1, 2, 3, 5)
 pp.printnv(a)
